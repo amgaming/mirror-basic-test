19eca0275959985e1b99901768dafeb31a2c6b43
Tsur <prucheta@gmail.com>
2020-11-02 20:51:26 +0100

    feat: add trap interaction

---
+++ Assets/Scripts/Player/Interact.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Interact : NetworkBehaviour
+{
+    [SerializeField]
+    [Tooltip("The player prefab camera. A reference to optimize performance")]
+    private Camera camera;
+    [SerializeField]
+    [Tooltip("All the object the user can interact with based on the layer the objects are placed")]
+    private LayerMask layerMask;
+    [SerializeField]
+    [Tooltip("The interaction cool down time")]
+    private float interactionTime = 2f;
+    private float currentInteractionTimeElapsed = 0f;
+    private Item itemToInteractWith;
+
+    private void Update()
+    {
+
+        if(!isLocalPlayer){ 
+            return;
+        }
+
+        selectItemFromCameraRay();
+
+        if(HasSelectedItem()){
+
+            // Display UI Message
+            // uiText.SetActive(true);
+
+            if(Input.GetKey(KeyCode.E)){
+                IncrementInteractionTime();
+            }
+            else {
+                currentInteractionTimeElapsed = 0f;
+            }
+
+            // UpdateProgressImage();
+        }
+        else {
+
+            // uiText.SetActive(false);
+            currentInteractionTimeElapsed = 0f;
+        }
+    }
+
+    private bool HasSelectedItem(){
+        return itemToInteractWith != null;
+    }
+
+    private void IncrementInteractionTime(){
+        currentInteractionTimeElapsed += Time.deltaTime;
+        if(currentInteractionTimeElapsed >= interactionTime){
+            MoveItemToInventory();
+        }
+    }
+
+    // private void UpdateProgressImage(){
+    //     float percentage = currentInteractionTimeElapsed / interactionTime;
+    //     progressImage.fillAmount = percentage;
+    // }
+
+    private void MoveItemToInventory(){
+        Destroy(itemToInteractWith.gameObject);
+        itemToInteractWith = null;
+    }
+
+    private void selectItemFromCameraRay()
+    {
+        Ray ray = camera.ViewportPointToRay(Vector3.one / 2.0f);
+        // Debug.DrawRay(ray.origin, ray.direction * 2.0f, Color.red);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 2.0f, layerMask)){
+            var itemFound = hit.collider.GetComponent<Item>();
+            if(itemFound != null && itemFound != itemToInteractWith){
+                itemToInteractWith = itemFound;
+            }
+            else{
+                itemFound = null;
+            }
+        } 
+        else{
+            itemToInteractWith = null;
+        }
+    }
+
+}
