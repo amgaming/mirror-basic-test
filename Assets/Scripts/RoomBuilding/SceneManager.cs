8b1c318f0262e7e877a69c6d3212bceefdd915ed
Antonio <acamposruiz@gmail.com>
2020-11-21 14:07:45 +0100

    feat: roombuilding start point

---
+++ Assets/Scripts/RoomBuilding/SceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject[] rooms;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* GameObject room = GameObject.FindWithTag("Room1");
+        Instantiate(room);
+        room.transform.position = Vector3.zero; */
+        int roomId = Random.Range(0, rooms.Length);
+        GameObject room = rooms[roomId];
+        Instantiate(room);
+        room.transform.position = Vector3.zero;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
