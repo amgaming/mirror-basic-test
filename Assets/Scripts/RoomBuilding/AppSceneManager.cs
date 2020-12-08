62834800a44da85bbbc6767180a0c237c7177364
Tsur <prucheta@gmail.com>
2020-12-08 20:12:15 +0100

    feat: add traps to room building"

--- Assets/Scripts/RoomBuilding/AppSceneManager.cs
+++ Assets/Scripts/RoomBuilding/AppSceneManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AppSceneManager : MonoBehaviour
 {
     public GameObject[] rooms;
+    public static GameObject room;
+    public static int roomIndex;
     public Camera _camera;
     // Start is called before the first frame update
     void Start()
@@ -12,17 +14,11 @@ public class AppSceneManager : MonoBehaviour
         /* GameObject room = GameObject.FindWithTag("Room1");
         Instantiate(room);
         room.transform.position = Vector3.zero; */
-        int roomId = Random.Range(0, rooms.Length);
-        GameObject room = rooms[roomId];
-        Instantiate(room);
+        roomIndex = Random.Range(0, rooms.Length);
+        room = Instantiate(rooms[roomIndex]);
         room.transform.position = Vector3.zero;
-        _camera.GetComponent<CameraController>().room = room;
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
 }
