e250b1dbe6c7d2cbaf8971d6df30f86d05c962a8
Antonio <acamposruiz@gmail.com>
2020-12-22 15:36:24 +0100

    feat: mirror to gameroom building

---
+++ Assets/Scripts/RoomBuilding/PreloadRoomBuilding.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+
+public class PreloadRoomBuilding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("RoomBuilding");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
