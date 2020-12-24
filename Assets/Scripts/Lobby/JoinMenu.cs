bd8f2169fa7a244eabee780a93b7d001f99a35f0
zuripabon <zurisadai.pabon@gmail.com>
2020-12-24 16:31:32 +0100

    feat: lobby screen

---
+++ Assets/Scripts/Lobby/JoinMenu.cs
@@ -0,0 +1,48 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoinMenu : MonoBehaviour 
+{
+    [SerializeField] private NetworkLobbyManager networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+    [SerializeField] private TMP_InputField ipAddressInputField = null;
+    [SerializeField] private Button joinButton = null;
+
+    private void OnEnable()
+    {
+        NetworkLobbyManager.OnClientConnected += HandleClientConnected;
+        NetworkLobbyManager.OnClientDisconnected += HandleClientDisconnected;
+    }
+
+    private void OnDisable()
+    {
+        NetworkLobbyManager.OnClientConnected -= HandleClientConnected;
+        NetworkLobbyManager.OnClientDisconnected -= HandleClientDisconnected;
+    }
+
+    public void JoinLobby()
+    {
+        string ipAddress = ipAddressInputField.text;
+
+        networkManager.networkAddress = ipAddress;
+        networkManager.StartClient();
+
+        joinButton.interactable = false;
+    }
+
+    private void HandleClientConnected()
+    {
+        joinButton.interactable = true;
+
+        gameObject.SetActive(false);
+        landingPagePanel.SetActive(false);
+    }
+
+    private void HandleClientDisconnected()
+    {
+        joinButton.interactable = true;
+    }
+}
---
+++ Assets/Scripts/Lobby/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+ 
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private NetworkLobbyManager networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+    
+    [Header("UI")]
+    [SerializeField] private GameObject lobbyPagePanel = null;
+
+    public void HostLobby()
+    {
+        networkManager.StartHost();
+
+        landingPagePanel.SetActive(false);
+        lobbyPagePanel.SetActive(true);
+    }
+}
