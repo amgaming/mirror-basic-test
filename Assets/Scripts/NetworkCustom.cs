bdebc0ec70089d6ad8c0ea51a4408b2dfa4a4039
Antonio <acamposruiz@gmail.com>
2020-12-27 23:34:15 +0100

    wip

---
+++ Assets/Scripts/NetworkCustom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using Mirror;
+
+public class NetworkCustom : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+     
+        Debug.Log("OnClientConnect Called");
+ 
+        base.OnClientConnect(conn);
+    }
+}
