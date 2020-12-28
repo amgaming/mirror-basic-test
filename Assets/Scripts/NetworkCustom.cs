4407b6d4c7244dc1c7db96279eca70936350465e
Antonio <acamposruiz@gmail.com>
2020-12-28 03:02:17 +0100

    wip

--- Assets/Scripts/NetworkCustom.cs
+++ Assets/Scripts/NetworkCustom.cs
@@ -16,7 +16,9 @@ public class NetworkCustom : NetworkManager
         base.OnClientConnect(conn);
 
         //GamePlayerMng.users.Add(GameObject.Find("PlayerData").GetComponent<GamePlayerMng>().getUser());
-        List<ListUser> users = GameObject.Find("GamePlayerMngGnrlObject").GetComponentInChildren<GamePlayerMngGnrl>().users;
+        GameObject gamePlayerMngGnrlObject = GameObject.Find("GamePlayerMngGnrlObject");
+        GamePlayerMngGnrl gamePlayerMngGnrl = gamePlayerMngGnrlObject.GetComponentInChildren<GamePlayerMngGnrl>();
+        List<ListUser> users = gamePlayerMngGnrl.users;
         users.Add(GameObject.Find("PlayerData").GetComponent<GamePlayerMng>().getUser());
         GameObject.Find("UsersListTextData").GetComponentInChildren<Text>().text = users.Count.ToString();
     }
